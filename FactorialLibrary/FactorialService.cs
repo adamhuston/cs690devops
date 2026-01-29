23a7fe02377b2109aac4eb2c523e38c774f6dbee
adamhuston <adam.huston@bsu.edu>
2026-01-29 01:46:57 +0000

    Negative test

--- FactorialLibrary/FactorialService.cs
+++ FactorialLibrary/FactorialService.cs
@@ -1,8 +1,11 @@
-﻿namespace FactorialService;
+﻿namespace FactorialLibrary;
 
 public class FactorialService
 {
     public static int Factorial(int n) {
+        if (n<0) {
+            throw new InvalidOperationException("Negative numbers don't have proper factorial");
+        }
         if(n==1 || n==0) {
             return 1;
         }
